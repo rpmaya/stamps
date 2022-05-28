b8158df8c851027b759be539aa78d3cd93fbfc29
rpmaya <35469068+rpmaya@users.noreply.github.com>
2022-05-28 12:53:58 +0200

    Update create-metadata-script.js

--- create-metadata-script.js
+++ create-metadata-script.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-const IPFS_DIR = "QmRNZcRWcwP6nARU6whW4dDCKMDjoXsXECEwgUCDYnPDaX";
-const COLLECTION = 1;
-const MAX = 9;
+const CID = "QmcTrRsvDm18qbrgodXAcrbQktWtA7eiYtixBaz42ovpWY";
+
+const MAX = 144;
 //const MAXF = 2;
 
 function dec2hex(n) {
@@ -21,55 +21,104 @@ function getColumn(n) {
     return y;
 }
 */
-async function main() {  
+async function main() {
 
     for (var i = 1; i <= MAX; i++) {
 
-        const name = "metadata/" + dec2hex((COLLECTION-1)*MAX + i) + ".json";
+        const name = "metadata/" + dec2hex(i) + ".json";
 
-        var atts = [
-            {
-                "trait_type": "Background",
-                "value": "Blue"
-            },
-            {
-                "colection_type": "Number",
-                "value": COLLECTION
-            }
+        let backgroundColor;
+        let headColor;
+        let letter = "A";
 
-        ]
-/*
-        if (i === 2) {
-            const att = {"trait_type":"Noise", "value":"small"};
-            atts.push(att);
-        } else if (i === 3) {
-            const att = {"trait_type":"Noise", "value":"big"};
-            atts.push(att);
-        } else {
-            const att = {"trait_type":"Noise", "value":"normal"};
-            atts.push(att);
+        if (i > 0 && i <= 9) {
+            backgroundColor = "blue light";
+            headColor = "white";
+        } else if (i > 9 && i <= 18) {
+            backgroundColor = "strong Pink";
+            headColor = "red dark";
+        } else if (i > 18 && i <= 27) {
+            backgroundColor = 'lila';
+            headColor = 'strong lila';
+        } else if (i > 28 && i <= 36) {
+            backgroundColor = 'soft red';
+            headColor = 'green-yellow';
+        } else if (i > 36 && i <= 45) {
+            backgroundColor = 'orange';
+            headColor = 'lila-yellow';
+        } else if (i > 45 && i <= 54) {
+            backgroundColor = 'deep blue';
+            headColor = 'dark pink';
+        } else if (i > 55 && i <= 63) {
+            backgroundColor = 'pop pink';
+            headColor = 'soft pink-yellow';
+        }else if (i > 63 && i <= 72) {
+            backgroundColor = 'yellow';
+            headColor = 'blue-yellow';
+        } else if (i > 72 && i <= 81) {
+            backgroundColor = 'blue sky';
+            headColor = 'yellow-pink';
+        } else if (i > 81 && i <= 90) {
+            backgroundColor = 'blue';
+            headColor = 'red';
+        } else if (i > 90 && i <= 99) {
+            backgroundColor = 'light orange';
+            headColor = 'red blue';
+        } else if (i > 99 && i <= 108) {
+            backgroundColor = 'strong red';
+            headColor = 'black white';
+        } else if (i > 108 && i <= 117) {
+            backgroundColor = 'light green';
+            headColor = 'soft orange';
+        } else if (i > 117 && i <= 126) {
+            backgroundColor = 'green pond';
+            headColor = 'strawberry pink';
+        } else if (i > 126 && i <= 135) {
+            backgroundColor = 'yellow green';
+            headColor = 'red green';
+        } else if (i > 135 && i <= 144) {
+            backgroundColor = 'blueberry';
+            headColor = 'blue orange';
         }
-
-        const attF = {"pos_type":  "File", "value": getFile(i)};
-        const attC = {"pos_type": "Column", "value": getColumn(i)};
+/*
+        const attF = { "pos_type": "File", "value": getFile(i) };
+        const attC = { "pos_type": "Column", "value": getColumn(i) };
         atts.push(attF);
         atts.push(attC);
 */
-        
-        nft = {
-                description: 'Test Collection ' + COLLECTION + ' Image: ' + i, 
-                image: 'https://gateway.pinata.cloud/ipfs/' + IPFS_DIR + '/A' + i + '.JPG',
-                name: 'name '+ i + ' colection ' + COLLECTION,
-                attributes: atts
+        var atts = [
+            {
+                "color_type": "Background",
+                "value": backgroundColor
+            },
+            {
+                "color_type": "Head",
+                "value": headColor
+            },
+            {
+                "alfanum_type": "Letter",
+                "value": letter
+            },
+            {
+                "alfanum_type": "Number",
+                "value": i % 9
+            }
+        ]
+
+        let nft = {
+            description: 'Proof Collection Image: ' + i,
+            image: 'https://gateway.pinata.cloud/ipfs/' + CID + '/' + i + '.jpg',
+            name: 'Proof ' + i,
+            attributes: atts
         }
 
 
         const data = JSON.stringify(nft, null, 4);
 
-        try{
+        try {
             fs.writeFileSync(name, data);
             console.log("JSON NFT", i, "saved.");
-        }catch(error) {
+        } catch (error) {
             console.error(error);
         }
     }
