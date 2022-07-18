7798977205092a0d93c8b8f54f674081fb84092b
rpmaya <35469068+rpmaya@users.noreply.github.com>
2022-07-18 13:01:22 +0200

    Update deploy-collections-script.js

--- deploy-collections-script.js
+++ deploy-collections-script.js
@@ -4,33 +4,39 @@ async function main() {
 
 
     const [deployer] = await ethers.getSigners();
-    //console.log("Deploying contracts with the account:", deployer.address);
-    //let txHash, txReceipt;
-    const Collection = await ethers.getContractFactory("StampCollection");
+    console.log("Deploying contracts with the account:", deployer.address);
+    let txHash, txReceipt;
+    const Collection = await ethers.getContractFactory("CryptoPennyBlack");
 
-     /* Deploy Collections 721 Contract  */
+     /* Deploy Collections 721 Contract */
      const collection = await Collection.deploy();
      await collection.deployed()
      txHash = collection.deployTransaction.hash;
      txReceipt = await ethers.provider.waitForTransaction(txHash);
      let collectionContractAddress = txReceipt.contractAddress;
-     console.log("Soga Collection deployed to:", collectionContractAddress);
-    
+     console.log("Crypto Penny Black deployed to:", collectionContractAddress);
+   
 
-     const collectionAddress = collectionContractAddress;
-     //const collectionAddress = '0x77dFFFBE961D8bD7e50Ba33C0f474A24D1Ca8e93';
-     const contractCollection = Collection.attach(collectionAddress);
+     //const collectionAddress = collectionContractAddress;
+     //const collectionAddress = '0x6E9d756b3e63b149107cC31dd637846Bd21DBc96';
+     //const contractCollection = Collection.attach(collectionAddress);
 
-     /* Mint licenses  */
+     /* Mint licenses 
+     const maxNFT = 18;
      const CID = "QmeTg3YCRUBBeJqJKSjyqAcsEYDhQ4SuRaFxa3n7h7Qgt4";
-     const maxNFT = 16;
 
-     for (let i = 1; i <= maxNFT; i++) {
-        uri = CID + "/" + i + ".json";
-        const txm = await contractCollection.safeMint(deployer.address, uri);
-        await txm.wait();
-        console.log("Collection", uri, "minted");
+     //const tx = await contractCollection.mint(maxNFT, CID);
+     //await tx.wait();
+     //console.log("Collection completed");
+     
+     for (let i = 0; i < maxNFT; i++) {
+        //uri = CID + "/" + i + ".json";
+        //const txm = await contractCollection.safeMint(deployer.address, uri);
+        //await txm.wait();
+        const url = await contractCollection.tokenURI(i);
+        console.log("URL NFT", i, ":", url);
      }
+    */
     
      /* Testing
      const id = 0;
